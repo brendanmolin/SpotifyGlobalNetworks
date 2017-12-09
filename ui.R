54c62d0eacf0df517e4a7246c589c6ccfba6d43c
Brendan Molin - PC <brendan.molin@gmail.com>
2017-12-09 17:00:00 -0500

    touchups for publishing - real links and not included city network chart

--- ui.R
+++ ui.R
@@ -3,14 +3,14 @@ ui <-
     headerPanel("Global Music Network"),
     tags$div(class="header", checked=NA,
              tags$p(paste0("This graphic shows the similarity in musical taste between countries.  Spotify generates playlists for many cities across the world, specifically highlighting unique songs being listened to there. We aggregated the data by country and built a network of shared songs. To learn how the analysis was done, "),
-             a(href="shiny.rstudio.com/tutorial", "Click Here!"))
+             a(href="http://rpubs.com/bmolin/globalmusicnetworkpc", "Click Here!"))
     ),
     mainPanel(
       visNetworkOutput("network"),
       tableOutput("shiny_return"),
       uiOutput('music'),
       tags$div(class="header", checked=NA,
-               tags$a(href="shiny.rstudio.com/tutorial", "About the Author")
+               tags$a(href="https://www.linkedin.com/in/brendanmolin/", "About the Author")
       )
     )
   )
